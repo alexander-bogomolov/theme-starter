7a0067f719201e8264d94fe11a5b341fff905e26
abogomolov <alexander@bogomolov.de>
2013-11-12 10:04:54 +0100

    Add HTMLHint

--- command-line-tools/setup-grunt.sh
+++ command-line-tools/setup-grunt.sh
@@ -8,3 +8,4 @@ npm install grunt-contrib-qunit --save-dev
 npm install grunt-contrib-watch --save-dev
 npm install grunt-contrib-uglify --save-dev
 npm install grunt-concurrent --save-dev
+npm install grunt-htmlhint --save-dev
